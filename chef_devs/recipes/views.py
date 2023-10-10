22b36af6b90c08c2f92c7414c77ddb9f9b4745a0
DanielKibatullin <dkibatullin40@stuy.edu>
2023-10-10 14:21:12 -0400

    update

--- chef_devs/recipes/views.py
+++ chef_devs/recipes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+#Index 
+def index(request):
+
+#Cookies
+def cookies(request):
+    dico_cookies = request.COOKIES
