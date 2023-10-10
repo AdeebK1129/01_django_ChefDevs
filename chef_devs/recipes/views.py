435bdbae111c453a0534cadeeb2d6da8d5d5a0de
Samuel Li <xli40@stuy.edu>
2023-10-10 14:42:17 -0400

    added index method in views.py

--- chef_devs/recipes/views.py
+++ chef_devs/recipes/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+def index(request):
+   print("You are looking at recipes")
+   title_page = "Recipes"
+   recipe_list = [
+         {
+               "Name": "Wonton Soup", 
+               "Origin": "China",
+               "Ingredients": "X",
+               "Time to Cook": "X",
+          },
+          ]
+   
+   return render(request, "recipes/index.html",
+                  context={'title_page':title_page,
+                           'recipe_list':recipe_list})
