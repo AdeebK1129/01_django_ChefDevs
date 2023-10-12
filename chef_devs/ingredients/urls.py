8c13f5e98f3df12a784ace14f0c0d347238bacf7
DanielKibatullin <dkibatullin40@stuy.edu>
2023-10-12 15:01:34 -0400

    work

--- chef_devs/ingredients/urls.py
+++ chef_devs/ingredients/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/checkout', views.checkout, name='checkout'), 
-    path('/cart', views.cart, name='cart'),
+    #path('/checkout', views.checkout, name='checkout'), 
+    #path('/cart', views.cart, name='cart'),
 ]
