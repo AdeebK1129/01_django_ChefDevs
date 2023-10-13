cd2648cd94b90aafa899fb1f935c742482c95e9a
Adeeb <adeeb19909@gmail.com>
2023-10-13 00:15:15 -0400

    fixed merging conflicts and added redirection links

--- chef_devs/ingredients/urls.py
+++ chef_devs/ingredients/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'ingredients'  
+app_name = 'ingredients'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('checkout_info', views.checkout_info, name='checkout_info'), 
-    path('cart', views.cart, name='cart'),
+    path('checkout_info/', views.checkout_info, name='checkout_info'),  
+    path('cart/', views.cart, name='cart'),
     path('clear_cart/', views.clear_cart, name='clear_cart'),
 ]
