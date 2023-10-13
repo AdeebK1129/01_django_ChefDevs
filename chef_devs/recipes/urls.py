6a39df5f6a29e9f47aa8d24c045814c407efd2b8
Adeeb <adeeb19909@gmail.com>
2023-10-12 20:30:35 -0400

    fixing url bugs

--- chef_devs/recipes/urls.py
+++ chef_devs/recipes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('recipe_info/', views.recipe_info, name='recipe_info'), 
