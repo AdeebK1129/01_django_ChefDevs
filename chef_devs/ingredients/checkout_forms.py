30c46576c8bca002c1b67241568b3f8c1fb22282
Samuel Li <xli40@stuy.edu>
2023-10-13 14:02:04 -0400

    added text inside input boxes

--- chef_devs/ingredients/checkout_forms.py
+++ chef_devs/ingredients/checkout_forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class Checkout(forms.Form):
-    ingredient_name = forms.CharField(label='Ingredient Name', max_length=100)
+    ingredient_name = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Search...'}))
--- chef_devs/recipes/recipe_forms.py
+++ chef_devs/recipes/recipe_forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class RecipeForm(forms.Form):
-    food_name = forms.CharField(label='Food Name', max_length=100)
+    food_name = forms.CharField(label='', max_length=100,widget=forms.TextInput(attrs={'placeholder': 'Food Name'}))
 
 
 
